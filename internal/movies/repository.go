787e4b5e391bd23a8d20e290694bb53996d2dfa2
Lucas Silva <lucasfsilva2310@gmail.com>
2024-08-13 09:33:12 -0300

    create basic service and repository with GetAll func example

--- internal/movies/repository.go
+++ internal/movies/repository.go
@@ -5,3 +5,31 @@ import "database/sql"
 type Repository struct {
 	db *sql.DB
 }
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (repo *Repository) GetAll() ([]Movie, error) {
+	rows, err := repo.db.Query("SELECT * FROM movies")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var movies []Movie
+
+	for rows.Next() {
+		var movie Movie
+		if err := rows.Scan(&movie.ID, &movie.Title, &movie.Description, &movie.ReleaseDate, &movie.Tags, &movie.Platforms, &movie.CreatedAt, &movie.UpdatedAt); err != nil {
+			return nil, err
+		}
+		movies = append(movies, movie)
+	}
+
+	return movies, nil
+}
--- internal/movies/service.go
+++ internal/movies/service.go
@@ -1,1 +1,15 @@
 package movies
+
+type Service struct {
+	repository Repository
+}
+
+func NewService(Repository *Repository) *Service {
+	return &Service{
+		repository: *Repository,
+	}
+}
+
+func (service *Service) GetAll() ([]Movie, error) {
+	return service.repository.GetAll()
+}
